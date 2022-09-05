6a8c25eecba16e8ecbd2816c31e42a1718751c55
czewail <chanzewail@gmail.com>
2022-09-05 18:45:15 +0800

    chore

--- package_src/lib/src/item/item.dart
+++ package_src/lib/src/item/item.dart
@@ -13,15 +13,57 @@ class FluuiItem extends StatelessWidget {
 
   final double? height;
 
+  final Widget? prefix;
+
+  final Widget? media;
+
+  final bool showBorder;
+
   const FluuiItem(
       {Key? key,
       this.child,
       this.title,
       this.onPress,
       this.message,
+      this.prefix,
+      this.media,
       this.icon,
+      this.showBorder = true,
       this.height})
       : super(key: key);
+
+  _getChildren() {
+    List<Widget> widgets = [];
+    if (prefix != null) {
+      widgets.add(Padding(
+        padding: const EdgeInsets.only(right: 7),
+        child: prefix!,
+      ));
+    }
+    if (title != null) {
+      widgets.add(Expanded(
+          child: Text(
+        title!,
+        style: const TextStyle(fontSize: 15.0),
+      )));
+    }
+    if (message != null) {
+      widgets.add(Text(
+        message!,
+        style: const TextStyle(color: Colors.grey, fontSize: 13),
+      ));
+    }
+    if (media != null) {
+      widgets.add(media!);
+    }
+    if (icon != null) {
+      widgets.add(Icon(
+        icon!,
+        color: Colors.grey,
+      ));
+    }
+    return widgets;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +73,19 @@ class FluuiItem extends StatelessWidget {
       children: [
         Container(
           height: height,
-        )
+          child: Padding(
+              padding: const EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
+              child: GestureDetector(
+                  onTap: () {
+                    onPress != null && onPress!();
+                  },
+                  child: Row(children: _getChildren()))),
+        ),
+        showBorder
+            ? const Divider(
+                height: 1.0,
+              )
+            : Container()
       ],
     );
   }
